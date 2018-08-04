4ccad08de49db7c7c63015a60c59b65928068644
roba <varga.robert2@gmail.com>
2018-08-04 11:37:39 +0200

    introduce Lombok Project - add lombok to pom.xml - refactoring classes - add @Slf4j annotation to bootstrap and controller classes - add @EqualsAndHashCode annotation exclude due the bi-diractional references

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,8 +1,13 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
@@ -27,51 +32,10 @@ public class Ingredient {
         this.uom = uom;
     }
 
-
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
 }
