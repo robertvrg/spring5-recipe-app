ab1151927ab6481cc2dc685e0e49701445c798c7
roba <varga.robert2@gmail.com>
2018-08-18 17:27:39 +0200

    Customizing Error Messages with Message Source

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -107,13 +107,13 @@ public class RecipeControllerTest {
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
+                .param("cookTime", "3000")
 
         )
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("recipe"))
                 .andExpect(view().name("recipe/recipeform"));
     }
-
 
     @Test
     public void testGetUpdateView() throws Exception {
